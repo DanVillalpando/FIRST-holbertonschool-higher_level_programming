5b1936408fdcf6f3836d07779437ec6ce429adfc
DanVillalpando <danielvch5@gmail.com>
2022-01-18 20:29:11 -0800

    1)

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Define a new class """
+
 class Square:
     """ Defines an empty square"""
     pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Square """
+class Square:
+    """Private attribute"""
+    def __init__(self, size):
+        """Initializes the private object."""
+        self.__size = size
