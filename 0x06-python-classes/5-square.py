a51414df4949e013ce67e4a63f801931fcc05d6b
DanVillalpando <danielvch5@gmail.com>
2022-01-18 21:16:21 -0800

    4 & 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -25,3 +25,12 @@ class Square(object):
     def area(self):
         """Area"""
         return self.__size * self.__size
+
+    def my_print(self):
+        """ Prints #"""
+        if self.__size == 0:
+            print()
+        for i in range(0, self.__size):
+            for j in range(0, self.__size):
+                print("#", end="")
+            print()
